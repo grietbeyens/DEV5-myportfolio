512e4399dc80475e7e3607aed87d35e635b91d82
grietbeyens <beyensgriet@gmail.com>
2022-09-30 20:07:58 +0200

    feat: mark card as done

--- DEV5-Human-Bingo/src/Card.js
+++ DEV5-Human-Bingo/src/Card.js
@@ -7,27 +7,22 @@ export default class Card {
   }
 
   markDone(target) {
-    // to mark a card as done, we add a class .bingo__card--done to it
     // 🔥🔥🔥 TODO 5: mark or unmark (toggle) a bingo card when clicked
     console.log("Marking card as done");
     console.log(target);
-    // hint: use class .bingo__card--done
+    target.classList.toggle("bingo__card--done"); // to mark a card as done, we add a class .bingo__card--done to it
   }
 
   render(counter) {
-    // rendering the card to the screen is done by building up a string of HTML
-    // after that, we append the HTML to the DOM - check the index.html file to see what structure to use
-    //console.log("Rendering card...");
-
     // 🔥🔥🔥 TODO3: build the HTML element and append it to the DOM
     let board = document.querySelector(".bingo__board");
-    let card = document.createElement("div");
+    let card = document.createElement("div"); // rendering the card to the screen is done by building up a string of HTML
 
     card.dataset.number = counter + 1;
     card.classList.add("bingo__card");
     card.setAttribute("id", "bingo__card" + counter);
-    card.innerHTML = this.title;  
-    board.appendChild(card);
+    card.innerHTML = this.title;
+    board.appendChild(card); // after that, we append the HTML to the DOM - check the index.html file to see what structure to use
 
     // 🔥🔥🔥 TODO4: when we click an item, we want to check for winners and we want to save the selection to storage
     card.addEventListener("click", (e) => {
