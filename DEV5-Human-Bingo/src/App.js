c9da0437dc108df8e30061244132677377d25717
grietbeyens <beyensgriet@gmail.com>
2022-09-30 23:31:29 +0200

    fix: unnecessary import card

--- DEV5-Human-Bingo/src/App.js
+++ DEV5-Human-Bingo/src/App.js
@@ -1,5 +1,4 @@
 // import another class
 import Bingo from "./Bingo.js";
-import Card from "./Card.js";
 // kickstart the app by calling the constructor via the `new` keyword
 const bingo = new Bingo();
