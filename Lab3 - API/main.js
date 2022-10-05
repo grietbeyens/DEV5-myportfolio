385974d9ea2161420dee6e262b7a28bec55e017a
grietbeyens <beyensgriet@gmail.com>
2022-10-05 17:12:41 +0200

    fix: localstorage issues

--- Lab3 - API/main.js
+++ Lab3 - API/main.js
@@ -3,5 +3,9 @@ import './style.css';
 import Weather from './src/Weather.js';
 import Nasa from './src/Nasa.js';
 
-const weather = new Weather("0fd925d2ab43ff14d4105bd5ed37684b");
-const nasa = new Nasa("sbTPIuU0IrySPRwmnaUyumYTt8Ybb8tjWNatf91c");
+const weatherInstance = new Weather("0fd925d2ab43ff14d4105bd5ed37684b");
+const nasaInstance = new Nasa("sbTPIuU0IrySPRwmnaUyumYTt8Ybb8tjWNatf91c");
+
+// if(weather.displayWeather === "Clouds") {
+//     document.querySelector(".weather__main").innerText = "looks like todays weather is " + weather.displayWeather;
+// }
--- Lab3 - API/src/Nasa.js
+++ Lab3 - API/src/Nasa.js
@@ -1,17 +1,32 @@
 export default class Nasa {
   constructor(api_key) {
     this.apiKey = api_key;
-    this.getNasa();
+     if (
+       localStorage.getItem("nasa") &&
+       Date.now() - localStorage.getItem("timestamp") < 60000
+     ) {
+       // get weather from local storage
+       const nasaData = JSON.parse(localStorage.getItem("weather"));
+       this.displayNasa(nasaData);
+       console.log("Cached nasa");
+     } else {
+       this.getNasa();
+     }
   }
 
     getNasa() {
         const url = `https://api.nasa.gov/planetary/apod?api_key=${this.apiKey}`;
         fetch(url)
-            .then((response) => response.json())
-            .then((data) => {
-                this.displayNasa(data);
-                //console.log(data);
-            });
+          .then((response) => response.json())
+          .then((data) => {
+            //console.log(data);
+            //local storage string naar json en json naar string
+            localStorage.setItem("nasa", JSON.stringify(data));
+            // save timestamp
+            localStorage.setItem("timestamp", Date.now());
+            this.displayNasa(data);
+          });
+
     }
 
     displayNasa(data) {
