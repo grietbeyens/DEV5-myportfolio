e1c533b3380c94194a77e852883b15881bf43429
grietbeyens <beyensgriet@gmail.com>
2022-10-05 15:12:25 +0200

    feat: add api key and export Nasa class

--- Lab3 - API/main.js
+++ Lab3 - API/main.js
@@ -1,6 +1,7 @@
-//sbTPIuU0IrySPRwmnaUyumYTt8Ybb8tjWNatf91c
 //GET https://api.nasa.gov/planetary/apod
 import './style.css';
 import Weather from './src/Weather.js';
+import Nasa from './src/Nasa.js';
 
 const weather = new Weather("0fd925d2ab43ff14d4105bd5ed37684b");
+const nasa = new Nasa("sbTPIuU0IrySPRwmnaUyumYTt8Ybb8tjWNatf91c");
---
+++ Lab3 - API/src/Nasa.js
@@ -0,0 +1,7 @@
+export default class Nasa {
+  constructor(api_key) {
+    this.apiKey = api_key;
+  }
+
+
+}
