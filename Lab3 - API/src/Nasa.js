f7dbeeb172231a7f43d41b900b34ee24a5692622
grietbeyens <beyensgriet@gmail.com>
2022-10-05 15:40:58 +0200

    feat: fetch nasa data and display img or video

--- Lab3 - API/src/Nasa.js
+++ Lab3 - API/src/Nasa.js
@@ -1,7 +1,32 @@
 export default class Nasa {
   constructor(api_key) {
     this.apiKey = api_key;
+    this.getNasa();
   }
+
+    getNasa() {
+        const url = `https://api.nasa.gov/planetary/apod?api_key=${this.apiKey}`;
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                this.displayNasa(data);
+                //console.log(data);
+            });
+    }
+
+    displayNasa(data) {
+        //console.log(data.media_type);
+        if(data.media_type === "image") {
+        const img = document.createElement("img");
+        img.src = data.hdurl;
+        document.querySelector(".nasa__content").appendChild(img);
+        } else if (data.media_type === "video") {
+        const video = document.createElement("iframe");
+        video.src = data.url;
+        //video.classList.add(".nasa__video");
+        document.querySelector(".nasa__content").appendChild(video);
+        }
+    }
 
 
 }
