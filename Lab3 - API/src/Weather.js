1a73dd487bf475d03f47cc86627da222f884c647
grietbeyens <beyensgriet@gmail.com>
2022-10-05 13:58:30 +0200

    feat: add geolocation and log in console

--- Lab3 - API/src/Weather.js
+++ Lab3 - API/src/Weather.js
@@ -1,6 +1,27 @@
-export default class Weather{
-    constructor(){
-        this.apiKey = api_key;
+export default class Weather {
+  constructor(api_key) {
+    this.apiKey = api_key;
+    this.getLocation();
+  }
+
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getWeather.bind(this));
+    } else {
+      console.log("Geolocation is not supported by this browser.");
     }
-   
+  }
+
+  getWeather(position) {
+    console.log(position);
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
+  }
 }
