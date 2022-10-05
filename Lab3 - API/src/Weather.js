b5f252cc2e0191c37605aca4716105d82447e35a
grietbeyens <beyensgriet@gmail.com>
2022-10-05 14:58:11 +0200

    feat: add localstorage and display weather

--- Lab3 - API/src/Weather.js
+++ Lab3 - API/src/Weather.js
@@ -1,7 +1,18 @@
 export default class Weather {
   constructor(api_key) {
     this.apiKey = api_key;
-    this.getLocation();
+    // check if weather is in local storage
+    if (
+      localStorage.getItem("weather") &&
+      Date.now() - localStorage.getItem("timestamp") < 60000
+    ) {
+      // get weather from local storage
+      const weatherData = JSON.parse(localStorage.getItem("weather"));
+      this.displayWeather(weatherData);
+      console.log("Cached");
+    } else {
+      this.getLocation();
+    }
   }
 
   getLocation() {
@@ -16,12 +27,32 @@ export default class Weather {
     console.log(position);
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`;
 
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            console.log(data);
-            });
+          //console.log(data);
+          //local storage string naar json en json naar string
+          localStorage.setItem("weather", JSON.stringify(data));
+          // save timestamp
+          localStorage.setItem("timestamp", Date.now());
+
+          this.displayWeather(data);
+        });
   }
+
+    displayWeather(data) {
+        const weather = data.weather[0].main;
+        document.querySelector(".weather__main").innerText = weather;
+        const weather__discr = data.weather[0].description;
+        document.querySelector(".weather__description").innerText = weather__discr;
+        const icon = data.weather[0].icon;
+        
+        const img = document.createElement("img");
+        img.src= `http://openweathermap.org/img/w/${icon}.png`;
+        document.querySelector(".weather__icon").appendChild(img);
+
+    }
+
 }
