6de5493355292711564ab905cf8288e3db9c6e9b
grietbeyens <beyensgriet@gmail.com>
2022-10-08 11:25:09 +0200

    feat: add cachedweather

--- Lab3 - API/main.js
+++ Lab3 - API/main.js
@@ -1,11 +1,22 @@
-//GET https://api.nasa.gov/planetary/apod
-import './style.css';
-import Weather from './src/Weather.js';
-import Nasa from './src/Nasa.js';
+import "./style.css";
+import Weather from "./src/Weather.js";
+import Nasa from "./src/Nasa.js";
 
 const weatherInstance = new Weather("0fd925d2ab43ff14d4105bd5ed37684b");
+
+
 const nasaInstance = new Nasa("sbTPIuU0IrySPRwmnaUyumYTt8Ybb8tjWNatf91c");
 
-// if(weather.displayWeather === "Clouds") {
-//     document.querySelector(".weather__main").innerText = "looks like todays weather is " + weather.displayWeather;
+//hier code uitvoeren om shit te doen
+//weatherInstance.getWeather()
+// dan op basis van deze data  dus checken of cloudy
+// dan pas de nasaInstance.display shit doen
+// dat betekent dus ook dat in uw nasa de constructor niet per se de data moet ophalen
+//dus eenmaal je weet wat je moet tonen kan je ofwel displayNasa doen ofwel gewoon zelf iets tonen
+
+//console.log(weatherInstance.getWeather())
+//check if cloudy
+// if (test.data.weather[0].main == "Clouds") {
+//     nasaInstance.getNasa();
+//     nasaInstance.displayNasa();
 // }
--- Lab3 - API/src/Weather.js
+++ Lab3 - API/src/Weather.js
@@ -1,58 +1,64 @@
 export default class Weather {
   constructor(api_key) {
     this.apiKey = api_key;
+
     // check if weather is in local storage
     if (
       localStorage.getItem("weather") &&
       Date.now() - localStorage.getItem("timestamp") < 60000
     ) {
-      // get weather from local storage
-      const weatherData = JSON.parse(localStorage.getItem("weather"));
-      this.displayWeather(weatherData);
-      console.log("Cached");
+      this.getWeatherFromCache();
     } else {
-      this.getLocation();
+      this.getWeather();
     }
   }
 
-  getLocation() {
+  getWeatherFromCache() {
+    console.log("Weather - From cache");
+
+    // get weather from local storage
+    const weather = JSON.parse(localStorage.getItem("weather"));
+    this.displayWeather(weather);
+  }
+
+  getWeather() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.getWeather.bind(this));
+      navigator.geolocation.getCurrentPosition((position) =>
+        this.getWeatherFromApi(position)
+      );
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
   }
 
-  getWeather(position) {
-    console.log(position);
+  getWeatherFromApi(position) {
+    console.log("Weather - From api");
+
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`;
 
     fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          //console.log(data);
-          //local storage string naar json en json naar string
-          localStorage.setItem("weather", JSON.stringify(data));
-          // save timestamp
-          localStorage.setItem("timestamp", Date.now());
+      .then((response) => response.json())
+      .then((data) => {
+        //local storage string naar json en json naar string
+        localStorage.setItem("weather", JSON.stringify(data));
+        // save timestamp
+        localStorage.setItem("timestamp", Date.now());
 
-          this.displayWeather(data);
-        });
+        this.displayWeather(data);
+      });
   }
 
-    displayWeather(data) {
-        const weather = data.weather[0].main;
-        document.querySelector(".weather__main").innerText = weather;
-        const weather__discr = data.weather[0].description;
-        document.querySelector(".weather__description").innerText = weather__discr;
-        const icon = data.weather[0].icon;
-        
-        const img = document.createElement("img");
-        img.src= `http://openweathermap.org/img/w/${icon}.png`;
-        document.querySelector(".weather__icon").appendChild(img);
+  displayWeather(data) {
+    const weather = data.weather[0].main;
+    document.querySelector(".weather__main").innerText = weather;
+    const weather__discr = data.weather[0].description;
+    document.querySelector(".weather__description").innerText = weather__discr;
+    const icon = data.weather[0].icon;
 
-    }
-
+    const img = document.createElement("img");
+    img.src = `http://openweathermap.org/img/w/${icon}.png`;
+    document.querySelector(".weather__icon").appendChild(img);
+  }
 }
