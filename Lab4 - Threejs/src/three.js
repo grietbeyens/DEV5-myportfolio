00760307850f7c6b9172c6a1d1b8dd509ccf3057
grietbeyens <beyensgriet@gmail.com>
2022-10-15 15:30:35 +0200

    feat: add camera animation

--- Lab4 - Threejs/src/three.js
+++ Lab4 - Threejs/src/three.js
@@ -40,7 +40,7 @@ const chickenSkin = textureLoader.load("/models/chicken/textures/material_normal
 const gltfLoader = new GLTFLoader();
 
 //sky
-const sphereGeometry = new THREE.SphereGeometry(100, 32, 32);
+const sphereGeometry = new THREE.SphereGeometry(50, 32, 32);
 const sphereMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
 sphereMaterial.map = sky;
 sphereMaterial.side = THREE.BackSide;
@@ -149,23 +149,77 @@ loader.load("/fonts/coolvetica.json", function (font) {
 
 //load chicken gltf
 let chicken;
+const addChicken = (x, y, z) => {
 gltfLoader.load("/models/chicken/scene.gltf", (gltf) => {
   chicken = gltf.scene;  
-  // chicken.scale.set(1, 1, 1);
-  //color chicken
-  // chicken.traverse((o) => {
-  //   if (o.isMesh) {
-  //     // o.material.color.set(0x303030);
-  //     o.material.map = chickenSkin;
-  //   }});
+  chicken.scale.set(4, 4, 4);
+  chicken.position.set(x, y, z);
   scene.add(chicken);
 });
+};
+//load duck
+let duck;
+const addDuck = (x, y, z) => {
+gltfLoader.load("/models/duck/scene.gltf", (gltf) => {
+  duck = gltf.scene;  
+  duck.position.set(x, y, z);
+  duck.scale.set(4, 4, 4);
+  scene.add(duck);
+});
+}; 
 
-camera.position.z = 5;
+let pizza;
+gltfLoader.load("/models/pizza/scene.gltf", (gltf) => {
+  pizza = gltf.scene;
+  pizza.position.set(-0.7, -1, 0.2);
+  pizza.scale.set(6, 6, 6);
+  scene.add(pizza);
+});
+
+// looping chicken
+for(let i = 0; i < 20; i++) {
+  let sign = Math.random() < 0.5 ? -1 : 1;
+  const x = Math.random() * 30 * sign;
+
+  sign = Math.random() < 0.5 ? -1 : 1;
+  const y = Math.random() * 30 * sign;
+
+  sign = Math.random() < 0.5 ? -1 : 1;
+  const z = Math.random() * 30 * sign;
+  addChicken(x,y,z);
+};
+
+// looping duck in other random places
+for(let i = 0; i < 20; i++) {
+  let sign = Math.random() < 0.5 ? -1 : 1;
+  const x = Math.random() * 20 * sign;
+
+  sign = Math.random() < 0.5 ? -1 : 1;
+  const y = Math.random() * 20 * sign;
+
+  sign = Math.random() < 0.5 ? -1 : 1;
+  const z = Math.random() * 20 * sign;
+  addDuck(x,y,z);
+};
+
+camera.position.x = 10;
+camera.position.z = 15
+camera.position.y = 10;
 
 function animate() {
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+camera.position.x -= 0.03;
+camera.position.z -= 0.02;
+camera.position.y -= 0.03;
+if(camera.position.x < 0) {
+  camera.position.x = 0;
+  camera.position.y = 0;
+  if(camera.position.z < 4) {
+  camera.position.z = 4;}
+}
+camera.lookAt(scene.position);
+
+renderer.render(scene, camera);
 }
 
 animate();
