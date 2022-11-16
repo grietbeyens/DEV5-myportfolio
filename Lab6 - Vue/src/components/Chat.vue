da2db7ac571fd1bd202bfa2f35131a58a3c67c75
grietbeyens <beyensgriet@gmail.com>
2022-11-16 14:37:20 +0100

    feat: add new chat to array and display

--- Lab6 - Vue/src/components/Chat.vue
+++ Lab6 - Vue/src/components/Chat.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref, reactive } from 'vue';
 let chats = reactive({ chats: [] });
+let message = ref('');
 onMounted(() => {
     const api_url = "https://lab5-p379.onrender.com/api/v1/messages/";
     fetch(api_url)
@@ -10,6 +11,33 @@ onMounted(() => {
             console.log(chats);
         });
 });
+
+const addChat = () => {
+    console.log(message.value);
+    let data = {
+        user: "Griet👀",
+        text: message.value
+    };
+    fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            chats.chats.push({
+                _id: data.data.id,
+                user: data.data.user,
+                text: data.data.text
+            });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
 </script>
 
 <template>
@@ -17,13 +45,13 @@ onMounted(() => {
         <div class="chat">
             <ul>
                 <li v-for="chat in chats.chats" :key="chats.id">
-                    {{chat.user}}: {{chat.text}}
+                    <b>{{chat.user}}:</b> {{chat.text}}
                 </li>
             </ul>
         </div>
        <div class="input-container">
-            <input type="text" placeholder="Add comment">
-            <button>Post</button>
+            <input type="text" class="input"  placeholder="Add comment" v-model="message" >
+            <button @click="addChat">Post</button>
        </div>
     </div>
 </template>
@@ -35,7 +63,8 @@ onMounted(() => {
 }
 .chat{
     width: 100;
-    height: 50vh;
+    height: 60vh;
     border: 1px solid black;
+    overflow: scroll;
 }
 </style>
