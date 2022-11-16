695a47f5e46df9f6359913d266748a6a89967794
grietbeyens <beyensgriet@gmail.com>
2022-11-16 12:35:25 +0100

    feat: add template

--- Lab6 - Vue/src/components/Chat.vue
+++ Lab6 - Vue/src/components/Chat.vue
@@ -8,10 +8,23 @@ onMounted(() => {
 
 <template>
     <div>
-       chat
+        <div class="chat">
+        </div>
+       <div class="input-container">
+            <input type="text" placeholder="Add comment">
+            <button>Post</button>
+       </div>
     </div>
 </template>
 
 <style scoped>
-
+.input-container{
+    padding: 2em 1em;
+    background-color: aliceblue;
+}
+.chat{
+    width: 100;
+    height: 50vh;
+    border: 1px solid black;
+}
 </style>
